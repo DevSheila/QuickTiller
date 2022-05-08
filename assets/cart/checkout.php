af15b5c5d031dcbdbd95d7cb09bba34a2a3f6780
Mutukukioko <95979150+Mutukukioko@users.noreply.github.com>
2022-05-08 21:08:33 +0300

    Cart checkout modal

---
+++ assets/cart/checkout.php
@@ -0,0 +1,54 @@
+<div class="modal fade" id="modal-lg">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header bg-indigo">
+              <h4 class="modal-title">CHECKOUT</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <div class="border rounded ">
+            <div class="pb-2"></div>
+                            <div class="row ">
+                                <div class="col-md-1 offset-md-1 ">
+                                 <img src="assets/img/cart/3.jpg" alt="image1" class="img-fluid">
+                                 </div>
+                                    <div class="row">
+                                     <h5 class="pl-3">
+                                      Product1
+                                     </h5>
+                                     <h5 class="pl-3">Seller: AQT</h5>
+                                     <h5 class="pl-3">Ksh3000</h5>
+                                     
+                                    </div>
+
+                                    
+</div>
+<div class="pb-2"></div>
+ <div class="row ">
+                       
+<div class="col-md-1 offset-md-1 ">
+                                 <img src="assets/img/cart/3.jpg" alt="image1" class="img-fluid">
+                                 </div>
+                                    <div class="row ">
+                                     <h5 class="pl-3">
+                                      Product1
+                                     </h5>
+                                     <h5 class="pl-3">Seller: AQT</h5>
+                                     <h5 class="pl-3">Ksh3000</h5>
+                                     
+                                    </div>
+</div>
+
+
+            </div>
+            <div class="modal-footer justify-content-between bg-indigo">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
