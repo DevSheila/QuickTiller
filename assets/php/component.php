9a47ed1f46166590c26c435b9465f0ec1851cc80
Mutukukioko <95979150+Mutukukioko@users.noreply.github.com>
2022-05-18 23:57:09 +0300

    New ui for the Qr scan and user Ui

---
+++ assets/php/component.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+
+
+if (isset($_POST['add'])) {
+    # code...
+    if(isset($_SESSION['cart'])){
+          
+       $item_array_id= array_column($_SESSION['cart'],"qrvalue");
+     
+
+       if(in_array($_POST['qrvalue'],$item_array_id)){
+           echo "<script>alert('Product is already added in the cart...!')<script>";
+           header('location:../../cart.php');
+       }else{
+
+        $count=count($_SESSION['cart']);
+        $item_array=array(
+            'qrvalue'=>$_POST['qrvalue']
+        );
+
+        $_SESSION['cart'][$count]=$item_array;
+print_r($item_array);
+header('location:../../cart.php');
+        
+
+       }
+
+    }else{
+        $item_array=array(
+            'qrvalue'=>$_POST['qrvalue']
+        );
+
+        $_SESSION['cart'][0]=$item_array;
+      
+        print_r($_SESSION['cart']);
+        header('location:../../cart.php');
+    }
+}
+//cart removing and adding
+if (isset($_POST['remove'])) {
+  
+    if ($_GET['action']=='remove') {
+        
+        foreach ($_SESSION['cart'] as $key => $value ) {
+           
+            if ($value['qrvalue']==$_GET['id']) {
+                
+                echo $_GET['id'];
+                unset($_SESSION['cart'][$key]);
+                echo"<script>alert('Product has been removed....!)</script>";
+               
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
